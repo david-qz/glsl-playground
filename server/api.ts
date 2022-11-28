79453957a3fc24bba2bab3852bb9c0a55c808f41
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-11-27 20:51:48 -0800

    Handle not-founds for api routes separately
    Before we were sending index.html on all unmatched routes in order to
    enable client-side routing. This patch makes it so any not-found request that
    otherwise matches the api prefix will get sent an actual 404 response.
    
    This creates a better experience during testing, where the index.html
    doesn't actually exist.

--- server/api.ts
+++ server/api.ts
@@ -2,11 +2,16 @@
  * Routes here belong to the API. All routes here assume API_PREFIX. In local
  * development, this is assumed to be /api/v1. See the Webpack configuration.
  ******************************************************************************/
-import { Router } from 'express';
+import { Router, type Request, type Response, type NextFunction } from 'express';
 import usersController from './controllers/users-controller.js';
+import HttpError from './utils/http-error.js';
 
 const router = Router();
 
 router.use('/users', usersController);
+
+router.use('*', (request: Request, response: Response, next: NextFunction) => {
+  next(new HttpError(`'${request.originalUrl}' not found`, 404));
+});
 
 export default router;
