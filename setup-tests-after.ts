4c6c42e749fc2f4189f7ce3faf1c25ff6f77cf60
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-11-27 20:20:40 -0800

    Suppress logging below a warn level during testing

--- setup-tests-after.ts
+++ setup-tests-after.ts
@@ -4,13 +4,22 @@
  * available. This is a good place to put global setup and tear down.
  ******************************************************************************/
 import { afterAll } from '@jest/globals';
-import pool from './server/database.js';
 // These don't seem to work, but there's documentation stating to do this.
 // Perhaps a bug fix will one day make this operable again. Doesn't seem to hurt
 // anything. Keep around if matchers get fixed. See:
 // https://github.com/testing-library/jest-dom/issues/442
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+
+// Suppress any logging below a warn level during testing.
+global.console = {
+  ...global.console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+};
+
+import pool from './server/database.js';
 
 afterAll(() => pool.end());
 global.fetch = jest.fn();
