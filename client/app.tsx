da5b90825e3769ac7efb07f2e6057c9a76f3f31f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-11-29 01:39:01 -0800

    Sketch out auth form
    It doesn't do anything right now, but it looks alright.

--- client/app.tsx
+++ client/app.tsx
@@ -4,16 +4,26 @@ import './reset.css';
 import './global.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Editor from './components/editor/editor';
+import AuthForm from './components/auth-form/auth-form';
+import HeaderLayout from './components/header-layout/header-layout';
+import { AuthContextProvider } from './hooks/auth-context';
 
 const container = document.getElementById('app') || document.createElement('div');
 container.id = 'app';
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={ <Editor /> } />
-      </Routes>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Editor />} />
+          <Route element={<HeaderLayout />} >
+            <Route path="/auth/log-in" element={<AuthForm method='log-in' />} />
+            <Route path="/auth/sign-up" element={<AuthForm method='sign-up' />} />
+          </Route>
+          <Route path='*' element={'not found'} />
+        </Routes>
+      </Router>
+    </AuthContextProvider>
   </React.StrictMode>
 );
