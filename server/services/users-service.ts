2f32bda580128d6842f2b0d153f52da9e02732e2
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-12-08 12:37:48 -0800

    Validate credentials on the server as well

--- server/services/users-service.ts
+++ server/services/users-service.ts
@@ -3,9 +3,18 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/user-model.js';
 import HttpError from '../utils/http-error.js';
+import * as EmailValidator from 'email-validator';
 
 export async function create(email: string, password: string): Promise<User> {
   const passwordHash = await bcrypt.hash(password, environment.SALT_ROUNDS);
+
+  if (!EmailValidator.validate(email)) {
+    throw new HttpError('Not a valid email', 400);
+  }
+
+  if (password.length < 6) {
+    throw new HttpError('Password too short', 400);
+  }
 
   try {
     return await User.insert(email, passwordHash);
