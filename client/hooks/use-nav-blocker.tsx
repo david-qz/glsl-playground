71af7788fce3d098b1334c28cc1bc87f980d9050
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-12-15 09:27:37 -0800

    Handle edge case in navigation blocker

--- client/hooks/use-nav-blocker.tsx
+++ client/hooks/use-nav-blocker.tsx
@@ -2,7 +2,7 @@
  * Adapted from https://dev.to/tadasgo/block-user-navigation-with-react-router-v6-417f
  */
 import { type History } from "history";
-import { useContext, useEffect, useLayoutEffect, useRef } from "react";
+import { useContext, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { UNSAFE_NavigationContext } from "react-router-dom";
 
 export type NavBlockerControl = {
@@ -23,8 +23,13 @@ export function useNavBlocker(onBlock: NavBlocker, when: boolean = true): () => 
   });
 
   // A ref to the unblock function so we can pass it out of the hook
+  // If we're not blocking this should be a no-op function so the consumer doesn't have to type narrow
   const unblockRef = useRef(() => {});
-  const invalidateRef = useRef(0);
+
+  // This counter is used to force a re-render whenever the unblock function is called from outside the hook.
+  // In the case that unblock is called but the blocking condition doesn't change, the hook wouldn't rerun without this.
+  // This is an edge-case. In most circumstances unblock should be called immediately before navigating away.
+  const [counter, setCounter] = useState<number>(0);
 
   useEffect(() => {
     if (!when) {
@@ -54,12 +59,11 @@ export function useNavBlocker(onBlock: NavBlocker, when: boolean = true): () => 
 
     unblockRef.current = () => {
       unblock();
-      invalidateRef.current++;
+      setCounter((c) => c + 1);
     };
 
     return unblock;
-  }, [block, when, invalidateRef.current]);
+  }, [block, when, counter]);
 
-  // If we're not blocking just pass a no-op so the consumer doesn't have to type narrow
   return unblockRef.current;
 }
