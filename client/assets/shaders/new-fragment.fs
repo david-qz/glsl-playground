c7a3f35c52ac04f3b3b7c63ac1f71920a2bd5d3f
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-12-15 18:11:29 -0800

    Clarify names in example shader

--- client/assets/shaders/new-fragment.fs
+++ client/assets/shaders/new-fragment.fs
@@ -9,18 +9,18 @@ out vec4 fragment_color;
 const vec3 materialColor = vec3(1.0, 1.0, 1.0);
 
 // Ambient lighting factors.
-const vec3 ambientColor = vec3(1.0, 1.0, 1.0);
+const vec3 ambientLightColor = vec3(1.0, 1.0, 1.0);
 const float ambientIntensity = 0.2;
 
 // Diffuse lighting factors.
-const vec3 diffuseColor = vec3(1.0, 1.0, 1.0);
+const vec3 diffuseLightColor = vec3(1.0, 1.0, 1.0);
 const vec3 lightDirection = normalize(vec3(1.0, 1.0, 1.0));
 
 void main() {
-  vec3 ambient = ambientColor * ambientIntensity;
+  vec3 ambient = ambientLightColor * ambientIntensity;
 
   float diffuseIntensity = max(dot(normal, lightDirection), 0.0);
-  vec3 diffuse = diffuseColor * diffuseIntensity;
+  vec3 diffuse = diffuseLightColor * diffuseIntensity;
 
   fragment_color = vec4((ambient + diffuse) * materialColor, 1.0);
 }
