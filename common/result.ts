b206c2f6430c155e2d90345fdc5c3ec04e6bc71b
David Quennoz <105565781+david-qz@users.noreply.github.com>
2023-06-02 14:00:13 -0700

    Just return the boolean :)

--- common/result.ts
+++ common/result.ts
@@ -1,8 +1,5 @@
 export type Result<T> = T | Error;
 
 export function isError<T>(e: Result<T>): e is Error {
-  if (e instanceof Error) {
-    return true;
-  }
-  return false;
+  return e instanceof Error;
 }
