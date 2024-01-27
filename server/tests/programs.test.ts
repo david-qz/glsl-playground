2d7eca5dd54ca2ec9222c818aa03ba29a06b661e
David Quennoz <105565781+david-qz@users.noreply.github.com>
2024-01-26 20:36:34 -0800

    Add back tests
    Chai isn't as nice as jests expect matchers, but mocha is so much
    easier to deal with than jest that I think it's worth it.

--- server/tests/programs.test.ts
+++ server/tests/programs.test.ts
@@ -1,163 +1,160 @@
-// /* @jest-environment node */
-// import { describe, expect, it } from "@jest/globals";
-// import request from "supertest";
-// import app from "../app";
-// import { seedDatabase, setupDatabase } from "../database.js";
-// import { type UserCredentials, testUsers } from "./utils";
+/* @jest-environment node */
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import request from "supertest";
+import app from "../app";
+import { seedDatabase, setupDatabase } from "../database.js";
+import { type UserCredentials, testUsers } from "./utils";
 
-// describe("API /programs routes", () => {
-//   beforeEach(async () => {
-//     await setupDatabase();
-//     await seedDatabase();
-//   });
+describe("API /programs routes", () => {
+  beforeEach(async () => {
+    await setupDatabase();
+    await seedDatabase();
+  });
 
-//   it("GET /programs/:id should return a program", async () => {
-//     const response = await request(app).get("/programs/1");
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toEqual({
-//       id: expect.any(String),
-//       userId: expect.any(String),
-//       title: expect.any(String),
-//       vertexSource: expect.any(String),
-//       fragmentSource: expect.any(String),
-//       didCompile: expect.any(Boolean),
-//       createdAt: expect.any(String),
-//       modifiedAt: expect.any(String),
-//     });
-//   });
+  it("GET /programs/:id should return a program", async () => {
+    const response = await request(app).get("/programs/1");
+    expect(response.status).equals(200);
+    expectProgram(response.body);
+  });
 
-//   it("GET /programs/:id should return 404 for invalid id", async () => {
-//     const response = await request(app).get("/programs/999999");
-//     expect(response.status).toEqual(404);
-//   });
+  it("GET /programs/:id should return 404 for invalid id", async () => {
+    const response = await request(app).get("/programs/999999");
+    expect(response.status).equals(404);
+  });
 
-//   it("GET /programs should return all the user's programs", async () => {
-//     const credentials: UserCredentials = testUsers.existing;
+  it("GET /programs should return all the user's programs", async () => {
+    const credentials: UserCredentials = testUsers.existing;
 
-//     // Log in
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(credentials);
+    // Log in
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(credentials);
 
-//     // Get programs
-//     const response = await agent.get("/programs");
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toBeInstanceOf(Array);
-//     expect(response.body[0]).toEqual({
-//       id: expect.any(String),
-//       userId: expect.any(String),
-//       title: expect.any(String),
-//       vertexSource: expect.any(String),
-//       fragmentSource: expect.any(String),
-//       didCompile: expect.any(Boolean),
-//       createdAt: expect.any(String),
-//       modifiedAt: expect.any(String),
-//     });
-//   });
+    // Get programs
+    const response = await agent.get("/programs");
+    expect(response.status).equals(200);
+    expect(response.body).is.an("array");
+    expectProgram(response.body[0]);
+  });
 
-//   it("POST /programs should create and return a new program", async () => {
-//     const credentials: UserCredentials = testUsers.existing;
+  it("POST /programs should create and return a new program", async () => {
+    const credentials: UserCredentials = testUsers.existing;
 
-//     // Log in
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(credentials);
+    // Log in
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(credentials);
 
-//     const newProgramData = {
-//       title: "a new program",
-//       vertexSource: "bahfjasdhlkjg",
-//       fragmentSource: "ajhsdfliuhefa",
-//       didCompile: false,
-//     };
+    const newProgramData = {
+      title: "a new program",
+      vertexSource: "bahfjasdhlkjg",
+      fragmentSource: "ajhsdfliuhefa",
+      didCompile: false,
+    };
 
-//     const response = await agent.post("/programs").send(newProgramData);
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toEqual({
-//       id: expect.any(String),
-//       userId: expect.any(String),
-//       title: newProgramData.title,
-//       vertexSource: newProgramData.vertexSource,
-//       fragmentSource: newProgramData.fragmentSource,
-//       didCompile: false,
-//       createdAt: expect.any(String),
-//       modifiedAt: expect.any(String),
-//     });
-//   });
+    const response = await agent.post("/programs").send(newProgramData);
+    expect(response.status).equals(200);
 
-//   it("POST /programs requires authentication", async () => {
-//     const newProgramData = {
-//       title: "a new program",
-//       vertexSource: "bahfjasdhlkjg",
-//       fragmentSource: "ajhsdfliuhefa",
-//       didCompile: false,
-//     };
+    const program = response.body;
+    expectProgram(program);
+    expect(program.title).equals(newProgramData.title);
+    expect(program.vertexSource).equals(newProgramData.vertexSource);
+    expect(program.fragmentSource).equals(newProgramData.fragmentSource);
+    expect(program.didCompile).equals(newProgramData.didCompile);
+  });
 
-//     const response = await request(app).post("/programs").send(newProgramData);
-//     expect(response.status).toEqual(401);
-//   });
+  it("POST /programs requires authentication", async () => {
+    const newProgramData = {
+      title: "a new program",
+      vertexSource: "bahfjasdhlkjg",
+      fragmentSource: "ajhsdfliuhefa",
+      didCompile: false,
+    };
 
-//   it("PATCH /programs/:id should update a program", async () => {
-//     const credentials: UserCredentials = testUsers.existing;
+    const response = await request(app).post("/programs").send(newProgramData);
+    expect(response.status).equals(401);
+  });
 
-//     // Log in
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(credentials);
+  it("PATCH /programs/:id should update a program", async () => {
+    const credentials: UserCredentials = testUsers.existing;
 
-//     // Get some updated data
-//     const updateData = {
-//       vertexSource: "new vertex code",
-//       fragmentSource: "new fragment code",
-//     };
+    // Log in
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(credentials);
 
-//     // Do the update
-//     const response = await agent.patch("/programs/1").send(updateData);
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toEqual({
-//       id: expect.any(String),
-//       userId: expect.any(String),
-//       title: expect.any(String),
-//       vertexSource: updateData.vertexSource,
-//       fragmentSource: updateData.fragmentSource,
-//       didCompile: expect.any(Boolean),
-//       createdAt: expect.any(String),
-//       modifiedAt: expect.any(String),
-//     });
-//   });
+    // Get some updated data
+    const updateData = {
+      vertexSource: "new vertex code",
+      fragmentSource: "new fragment code",
+    };
 
-//   it("PATCH /programs/:id shouldn't allow updating programs not owned by the current user", async () => {
-//     const credentials: UserCredentials = testUsers.troublesome;
+    // Do the update
+    const response = await agent.patch("/programs/1").send(updateData);
+    expect(response.status).equals(200);
 
-//     // Log in as the troublesome user
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(credentials);
+    const program = response.body;
+    expectProgram(program);
+    expect(program.vertexSource).equals(updateData.vertexSource);
+    expect(program.fragmentSource).equals(updateData.fragmentSource);
+  });
 
-//     // Try to modify another user's program
-//     const response = await agent.patch("/programs/1").send({ title: "user 1 smells" });
-//     expect(response.status).toEqual(403);
-//   });
+  it("PATCH /programs/:id shouldn't allow updating programs not owned by the current user", async () => {
+    const credentials: UserCredentials = testUsers.troublesome;
 
-//   it("DELETE /programs/:id should delete a program", async () => {
-//     const credentials: UserCredentials = testUsers.existing;
+    // Log in as the troublesome user
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(credentials);
 
-//     // Log in
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(credentials);
+    // Try to modify another user's program
+    const response = await agent.patch("/programs/1").send({ title: "user 1 smells" });
+    expect(response.status).equals(403);
+  });
 
-//     // Delete the program
-//     const deleteResponse = await agent.delete("/programs/1");
-//     expect(deleteResponse.status).toEqual(200);
+  it("DELETE /programs/:id should delete a program", async () => {
+    const credentials: UserCredentials = testUsers.existing;
 
-//     const getResponse = await agent.get("/programs/1");
-//     expect(getResponse.status).toEqual(404);
-//   });
+    // Log in
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(credentials);
 
-//   it("DELETE /programs/:id shouldn't allow users to delete programs they don't own", async () => {
-//     const credentials: UserCredentials = testUsers.troublesome;
+    // Delete the program
+    const deleteResponse = await agent.delete("/programs/1");
+    expect(deleteResponse.status).equals(200);
 
-//     // Log in as the troublesome user
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(credentials);
+    const getResponse = await agent.get("/programs/1");
+    expect(getResponse.status).equals(404);
+  });
 
-//     // Try to delete the program
-//     const deleteResponse = await agent.delete("/programs/1");
-//     expect(deleteResponse.status).toEqual(403);
-//   });
-// });
+  it("DELETE /programs/:id shouldn't allow users to delete programs they don't own", async () => {
+    const credentials: UserCredentials = testUsers.troublesome;
+
+    // Log in as the troublesome user
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(credentials);
+
+    // Try to delete the program
+    const deleteResponse = await agent.delete("/programs/1");
+    expect(deleteResponse.status).equals(403);
+  });
+});
+
+// biome-ignore lint/suspicious/noExplicitAny:
+function expectProgram(program: any) {
+  expect(program).to.have.keys(
+    "id",
+    "userId",
+    "title",
+    "vertexSource",
+    "fragmentSource",
+    "didCompile",
+    "createdAt",
+    "modifiedAt",
+  );
+  expect(program.id).is.a("string");
+  expect(program.userId).is.a("string");
+  expect(program.title).is.a("string");
+  expect(program.vertexSource).is.a("string");
+  expect(program.fragmentSource).is.a("string");
+  expect(program.didCompile).is.a("boolean");
+  expect(program.createdAt).is.a("string");
+  expect(program.modifiedAt).is.a("string");
+}
--- server/tests/users.test.ts
+++ server/tests/users.test.ts
@@ -1,104 +1,98 @@
-// /* @jest-environment node */
-// // The above comment necessary because express uses `setImmediate` which only exists in a node environment.
-// // FIXME: It would be much nicer if we could somehow direct jest to use the 'node' environment for all backend testing
-// //        while keeping the 'jsdom' environment for front end testing.
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import request from "supertest";
+import app from "../app";
+import { seedDatabase, setupDatabase } from "../database.js";
+import { CookieAccessInfo } from "cookiejar";
+import environment from "../environment";
+import { type UserCredentials, testUsers } from "./utils";
 
-// import { describe, expect, it } from "@jest/globals";
-// import request from "supertest";
-// import app from "../app";
-// import { seedDatabase, setupDatabase } from "../database.js";
-// import { CookieAccessInfo } from "cookiejar";
-// import environment from "../environment";
-// import { type UserCredentials, testUsers } from "./utils";
+describe("API /users routes", () => {
+  beforeEach(async () => {
+    await setupDatabase();
+    await seedDatabase();
+  });
 
-// describe("API /users routes", () => {
-//   beforeEach(async () => {
-//     await setupDatabase();
-//     await seedDatabase();
-//   });
+  it("POST /users should create a new user and log them in", async () => {
+    const newUser = testUsers.new;
 
-//   it("POST /users should create a new user and log them in", async () => {
-//     const newUser = testUsers.new;
+    // POST to route to create new user
+    const agent = request.agent(app);
+    const response = await agent.post("/users").send(newUser);
+    expect(response.status).equals(200);
 
-//     // POST to route to create new user
-//     const agent = request.agent(app);
-//     const response = await agent.post("/users").send(newUser);
-//     expect(response.status).toEqual(200);
+    // Should return user
+    const user = response.body;
+    expect(user).to.have.keys("id", "email");
+    expect(user.email).equals(newUser.email);
 
-//     // Should return user
-//     const user = response.body;
-//     expect(user).toEqual({
-//       id: expect.any(String),
-//       email: newUser.email,
-//     });
+    // Should create a session cookie
+    const session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
+    expect(session).not.to.be.undefined;
+  });
 
-//     // Should create a session cookie
-//     const session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
-//     expect(session).not.toBeUndefined();
-//   });
+  it("POST /users should error if email already exists", async () => {
+    // Some credentials with the same email as an existing user
+    const userCredentials: UserCredentials = { ...testUsers.existing, password: "blahblahblah" };
 
-//   it("POST /users should error if email already exists", async () => {
-//     // Some credentials with the same email as an existing user
-//     const userCredentials: UserCredentials = { ...testUsers.existing, password: "blahblahblah" };
+    // Expect sign-up request to fail
+    const response = await request(app).post("/users").send(userCredentials);
+    expect(response.status).equals(409);
+  });
 
-//     // Expect sign-up request to fail
-//     const response = await request(app).post("/users").send(userCredentials);
-//     expect(response.status).toEqual(409);
-//   });
+  it("GET /users/me should return the current user", async () => {
+    const userCredentials = testUsers.existing;
 
-//   it("GET /users/me should return the current user", async () => {
-//     const userCredentials = testUsers.existing;
+    // First Log in
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(userCredentials);
 
-//     // First Log in
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(userCredentials);
+    // Now get the current user
+    const response = await agent.get("/users/me");
+    expect(response.status).equals(200);
 
-//     // Now get the current user
-//     const response = await agent.get("/users/me");
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toEqual({
-//       id: expect.any(String),
-//       email: userCredentials.email,
-//     });
-//   });
+    const user = response.body;
+    expect(user).to.have.keys("id", "email");
+    expect(user.email).equals(userCredentials.email);
+  });
 
-//   it("GET /users/me should return 401 status if not logged in", async () => {
-//     // Get the current user without logging in
-//     const response = await request(app).get("/users/me");
-//     expect(response.status).toEqual(200);
-//     expect(response.body).toEqual(null);
-//   });
+  it("GET /users/me should return 401 status if not logged in", async () => {
+    // Get the current user without logging in
+    const response = await request(app).get("/users/me");
+    expect(response.status).equals(200);
+    expect(response.body).equals(null);
+  });
 
-//   it("POST /users/sessions should log a user in", async () => {
-//     const userCredentials = testUsers.existing;
+  it("POST /users/sessions should log a user in", async () => {
+    const userCredentials = testUsers.existing;
 
-//     // Request should be successful
-//     const agent = request.agent(app);
-//     const response = await agent.post("/users/sessions").send(userCredentials);
-//     expect(response.status).toEqual(200);
+    // Request should be successful
+    const agent = request.agent(app);
+    const response = await agent.post("/users/sessions").send(userCredentials);
+    expect(response.status).equals(200);
 
-//     // Should create a session cookie
-//     const session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
-//     expect(session).not.toBeUndefined();
-//   });
+    // Should create a session cookie
+    const session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
+    expect(session).not.to.be.undefined;
+  });
 
-//   it("DELETE /users/sessions should log a user out", async () => {
-//     const userCredentials = testUsers.existing;
+  it("DELETE /users/sessions should log a user out", async () => {
+    const userCredentials = testUsers.existing;
 
-//     // First log in
-//     const agent = request.agent(app);
-//     await agent.post("/users/sessions").send(userCredentials);
+    // First log in
+    const agent = request.agent(app);
+    await agent.post("/users/sessions").send(userCredentials);
 
-//     // Make sure we have a session
-//     let session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
-//     expect(session).not.toBeUndefined();
+    // Make sure we have a session
+    let session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
+    expect(session).not.to.be.undefined;
 
-//     // Now log out
-//     const response = await agent.delete("/users/sessions");
-//     expect(response.status).toEqual(200);
+    // Now log out
+    const response = await agent.delete("/users/sessions");
+    expect(response.status).equals(200);
 
-//     // Session cookie should be cleared
-//     session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
-//     expect(session).toBeUndefined();
-//   });
-// });
+    // Session cookie should be cleared
+    session = agent.jar.getCookie(environment.SESSION_COOKIE, CookieAccessInfo.All);
+    expect(session).to.be.undefined;
+  });
+});
