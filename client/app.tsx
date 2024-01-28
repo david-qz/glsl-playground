343ec0c9ac44a851490e2f85b9cd9a223918efcc
David Quennoz <105565781+david-qz@users.noreply.github.com>
2024-01-27 23:32:44 -0800

    Add some code splitting

--- client/app.tsx
+++ client/app.tsx
@@ -1,25 +1,68 @@
-import { type ReactElement } from "react";
+import { type ReactElement, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AuthForm from "./components/auth-form/auth-form";
-import Dashboard from "./components/dashboard/dashboard";
-import Editor from "./components/editor/editor";
 import HeaderLayout from "./components/header-layout/header-layout";
-import NotFound from "./components/not-found/not-found";
-import SaveProgram from "./components/save-program/save-program";
 import { AuthContextProvider } from "./hooks/use-auth-context";
+
+const Editor = lazy(() => import("./components/editor/editor"));
+const AuthForm = lazy(() => import("./components/auth-form/auth-form"));
+const Dashboard = lazy(() => import("./components/dashboard/dashboard"));
+const SaveProgram = lazy(() => import("./components/save-program/save-program"));
+const NotFound = lazy(() => import("./components/not-found/not-found"));
 
 export default function App(): ReactElement {
   return (
     <AuthContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Editor />} />
-          <Route path="/program/:id" element={<Editor />} />
+          <Route
+            path="/"
+            element={
+              <Suspense>
+                <Editor />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/program/:id"
+            element={
+              <Suspense>
+                <Editor />
+              </Suspense>
+            }
+          />
           <Route element={<HeaderLayout />}>
-            <Route path="/auth" element={<AuthForm />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/save-program" element={<SaveProgram />} />
-            <Route path="*" element={<NotFound />} />
+            <Route
+              path="/auth"
+              element={
+                <Suspense>
+                  <AuthForm />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <Suspense>
+                  <Dashboard />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/save-program"
+              element={
+                <Suspense>
+                  <SaveProgram />
+                </Suspense>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <Suspense>
+                  <NotFound />
+                </Suspense>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
